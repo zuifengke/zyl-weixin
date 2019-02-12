16b962d9da6d85139c63ec784eadf7c108006c35
zuifengke <yehui1989@qq.com>
2019-02-12 15:39:01 +0800

    提交
    提交代码修改

--- app.js
+++ app.js
@@ -2,34 +2,39 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        //发起网络请求
+        wx.request({
+          //这是我自己的java服务器的接口，将login（）获得的code发送的服务器换取session_key
+          url: 'https://abc.anzhonghui.xyz/WxtestServlet/UserController?method=getUnionId',
+          data: {
+            js_code: res.code,
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          method: 'GET',
+          success: function (res) {
+            console.log(res.data.session_key)
+            //拿到session_key实例化WXBizDataCrypt（）这个函数在下面解密用
+            var pc = new WXBizDataCrypt(appId, res.data.session_key)
+            wx.getUserInfo({
+              success: function (res) {
+                //拿到getUserInfo（）取得的res.encryptedData, res.iv，调用decryptData（）解密
+                var data = pc.decryptData(res.encryptedData, res.iv)
+                // data.unionId就是咱们要的东西了
+                app.globalData.unionid = data.unionId
+                console.log('解密后 unionid: ', app.globalData.unionid)
+              },
+              fail: function (res) {
+                console.log(res)
               }
-            }
-          })
-        }
+            })
+          },
+          fail: function (res) { },
+          complete: function (res) { }
+        });
       }
     })
   },
